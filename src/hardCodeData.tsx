81e770780fbc600721d4942b8242af5aa20d2879
Egor Fadin <70220336+asyalu@users.noreply.github.com>
2021-08-15 14:39:31 +0300

    feat: finish task React.Forms

---
+++ src/hardCodeData.tsx
@@ -0,0 +1,34 @@
+export const cards = [
+  {
+    name: 'Luncheon on the Boating Party',
+    artist: 'Pierre-Auguste Renoir',
+    when: '1880–1881',
+    likes: 756,
+    watched: 1134,
+    img: '../public/luncheon-on-the-boating-party.jpeg',
+  },
+  {
+    name: 'The Kiss',
+    artist: 'Gustav Klimt',
+    when: '1907–1908',
+    likes: 332,
+    watched: 712,
+    img: '../public/the-kiss-2.jpeg',
+  },
+  {
+    name: 'Napoleon crossing the Alps',
+    artist: 'Jacques-Louis David',
+    when: '1801',
+    likes: 899,
+    watched: 1312,
+    img: '../public/napoleon-crossing-the-alps-1.jpeg',
+  },
+  {
+    name: 'The Third of May',
+    artist: 'Francisco Goya',
+    when: '1814',
+    likes: 1233,
+    watched: 2012,
+    img: '../public/the-third-of-may-a-famous-painting-2.jpeg',
+  },
+];
